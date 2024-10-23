2efd9d13678aa6a30c89f59edb3e0926937a2d59
Alecit13 <u20221c488@upc.edu.pe>
2024-10-23 18:26:34 -0500

    feat: use to api Render

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:8080/api/v1'  // Ajusta la URL según sea necesario
+    apiUrl: 'https://jobhub-api-latest-ynn8.onrender.com/api/v1'  // Ajusta la URL según sea necesario
   };
   
