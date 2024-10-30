ed9c15906fc2db4d08156fc61dfe171a04451a54
Jimena <jimena.qn26@gmail.com>
2024-10-29 20:56:30 -0500

    chore: remove unused files

--- src/app/pages/profile/profile-layout/profile-layout.component.ts
+++ src/app/pages/profile/profile-layout/profile-layout.component.ts
@@ -1,13 +1,12 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { NavbarComponent } from '../../../shared/components/navbar/navbar.component';
-import { ProfileCardComponent } from '../profile-card/profile-card.component';
 import { ProfileBodyComponent } from '../profile-body/profile-body.component';
 import { FriendsSuggestComponent } from '../friends-suggest/friends-suggest.component';
 
 @Component({
   selector: 'app-profile-layout',
   standalone: true,
-  imports: [NavbarComponent, ProfileCardComponent, ProfileBodyComponent, FriendsSuggestComponent],
+  imports: [NavbarComponent, ProfileBodyComponent, FriendsSuggestComponent],
   templateUrl: './profile-layout.component.html',
   styleUrls: ['./profile-layout.component.css'],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]  // Añadir esto aquí
