d23e6a62e98cc505478c86b9001c36abe3baf91c
Alecit13 <u20221c488@upc.edu.pe>
2024-11-13 13:02:55 -0500

    "Preparar proyecto para despliegue"

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
   apiUrl: 'https://jobhub-api-latest-ynn8.onrender.com/api/v1',
-  paypalReturnUrl: 'https://jobhub-web/subscription/plans'
+  paypalReturnUrl: 'https://jobhub-web.netlify.app/subscription/plans'
   };
 
